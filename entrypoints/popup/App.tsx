f8027e02eacb0848e79c8285d9766e649da07262
Serhii Shramko <shramko.web@yahoo.com>
2025-01-25 17:01:04 -0500

    chore: project structure updates

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,14 +1,12 @@
-import { createSignal } from 'solid-js';
+import {createSignal} from 'solid-js';
 import "~/assets/tailwind.css";
 
 function App() {
-  const [count, setCount] = createSignal(0);
+    const [count, setCount] = createSignal(0);
 
-  return (
-    <>
-
-    </>
-  );
+    return (<>
+        <p>{count()}</p>
+    </>);
 }
 
 export default App;
